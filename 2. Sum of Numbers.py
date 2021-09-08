b6975cae8545638132dd39e497dd9f510c68fa0c
zagumionnov <zagumionnov6@gmail.com>
2021-09-08 13:58:56 +0300

    sum func

--- 2. Sum of Numbers.py
+++ 2. Sum of Numbers.py
@@ -14,6 +14,38 @@ get_sum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 """
 -2 -1 0 1 2 3 4 
 """
+
+"""
 def get_sum(a,b):
-    pass
-get_sum()
+    print([x for x in range(a, b+1)])
+
+
+
+get_sum(-2, 2)
+"""
+
+
+def get_sum(a, b):
+    if a <= b:
+        c = a
+        v = b
+    else:
+        v = a
+        c = b
+
+    z = 0
+    if a != b:
+        for i in range(c, v+1):
+            z += i
+    else:
+        z = a
+
+    return z
+
+
+print(get_sum(0, -1))
+
+"""
+def get_sum(a,b):
+    return sum(xrange(min(a,b), max(a,b)+1))
+"""
